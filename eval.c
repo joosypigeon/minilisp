f58dfa60ad1a4745400f0e82a03580ab141eb6df
joosypigeon <joosypigeon@googlemail.com>
2025-04-16 14:26:43 +0100

    Introduce eval_list

--- eval.c
+++ eval.c
@@ -86,12 +86,6 @@ Object *handle_lambda(Env *local, Object *expr) {
         exit(1);
     }
     Object *body = caddr(expr);
-    /*
-    if(body->type!=TYPE_PAIR && body->type!=TYPE_INT){
-        DEBUG_PRINT_ERROR("TYPE ERROR: expected function body\n");
-        exit(1);
-    }
-    */
     Object* lambda = make_lambda(params, body, env);
     return lambda;
 }
@@ -123,7 +117,7 @@ DispatchEntry special_forms[] = {
 
 Object *eval(Env *local, Object *expr);
 
-Object* eval_function(Object* lambda, Object* args){
+Object *eval_function(Object* lambda, Object* args){
     DEBUG_PRINT_VERBOSE("enter eval_function\n");
 
     Object* params = lambda->lambda.params;
@@ -157,137 +151,49 @@ Object* eval_function(Object* lambda, Object* args){
     return value;
 }
 
+Object *eval_list(Env *local, Object *expr) {
+    Object *first = car(expr);
+    if (first->type == TYPE_SYMBOL) {
+        for (int i = 0; special_forms[i].symbol != NULL; i++) {
+            if (strcmp(first->symbol, special_forms[i].symbol) == 0) {
+                return special_forms[i].handler(local, expr);
+            }
+        }
+        /* expect function call*/
+        Object *lambda = env_lookup(local, first->symbol);
+        if(lambda->type!=TYPE_LAMBDA){
+            DEBUG_PRINT_ERROR("TYPE ERROR: expected lambda expression\n");
+            exit(1);
+        }
+        Object* arg_list = cdr(expr);
+        Object* value = eval_function(lambda, arg_list);
+        return value;
+    }
+    /* Should have a lambda expression*/
+    if (first->type == TYPE_PAIR) {
+        Object* lambda = eval(local, first);
+        if(lambda->type!=TYPE_LAMBDA) {
+            DEBUG_PRINT_ERROR("TYPE ERROR: expected lambda expresions, got %s\n", type_to_string(lambda->type));
+            exit(1);               
+        }
+        Object *args = cdr(expr);
+        Object* value = eval_function(lambda, args);
+        return value;
+    }
+
+    printf("Cannot evaluate expression yet: ");
+    print_object(expr);
+    printf("\n");
+    exit(1);
+}
+
 Object *eval(Env *local, Object *expr) {
     if (expr->type == TYPE_INT || expr->type == TYPE_LAMBDA) {
         return expr;
     } else if (expr->type == TYPE_SYMBOL) {
         return env_lookup(local, expr->symbol);
     } else if (expr->type == TYPE_PAIR) {
-        Object *first = car(expr);
-        if (first->type == TYPE_SYMBOL) {
-            for (int i = 0; special_forms[i].symbol != NULL; i++) {
-                if (strcmp(first->symbol, special_forms[i].symbol) == 0) {
-                    return special_forms[i].handler(local, expr);
-                }
-            }
-/*
-            if (strcmp(first->symbol, SYM_QUOTE) == 0) {
-                return cadr(expr);
-            }
-            if (strcmp(first->symbol, SYM_DEFINE) == 0) {
-                Object *second = cadr(expr);
-                if (second->type == TYPE_SYMBOL) {
-                    Object *val = eval(local, caddr(expr));
-                    env_define(local, second->symbol, val);
-                    return val;
-                }
-                if(second->type==TYPE_PAIR){
-                    DEBUG_PRINT_VERBOSE("found function\n");
-                    Object *name = car(second);
-                    if(name->type!=TYPE_SYMBOL){
-                        DEBUG_PRINT_ERROR("TYPE ERROR: expected function name\n");
-                        exit(1);
-                    }
-                    Object* parameters = cdr(second);
-                    Object *body = caddr(expr);
-                    if(body->type!=TYPE_PAIR){
-                        DEBUG_PRINT_ERROR("TYPE ERROR: expected function body\n");
-                        exit(1);
-                    }
-                    Env *env = push_env(local);
-                    Object* lambda = make_lambda(parameters, body, env);
-
-                    env_define(local, name->symbol, lambda);
-
-                    return lambda;
-                }
-            }
-            if (strcmp(first->symbol, SYM_PLUS) == 0) {
-                int sum = 0;
-                Object *iter = cdr(expr);
-                while (iter != NIL) {
-                    Object *item = car(iter);      // extract current item
-                    Object *val = eval(local, item);   // evaluate
-                    if (val->type != TYPE_INT) {
-                        DEBUG_PRINT_ERROR("Type error: expected int\n");
-                        exit(1);
-                    }
-                    sum += val->int_val;
-                    iter = cdr(iter);              // move to next
-                }
-
-                Object* result = make_number(sum);
-                return result;
-            }
-            if (strcmp(first->symbol, SYM_TIMES) == 0) {
-                int sum = 1;
-                Object *iter = cdr(expr);
-                while (iter != NIL) {
-                    Object *item = car(iter);      // extract current item
-                    Object *val = eval(local, item);   // evaluate
-                    if (val->type != TYPE_INT) {
-                        DEBUG_PRINT_ERROR("Type error: expected int\n");
-                        exit(1);
-                    }
-                    sum *= val->int_val;
-                    iter = cdr(iter);              // move to next
-                }
-
-                Object* result = make_number(sum);
-                return result;
-            }  
-            if (strcmp(first->symbol, SYM_EQUAL) == 0) {
-                Object *second = eval(local, cadr(expr));
-                Object *third = eval(local, caddr(expr));
-                return lisp_equal(second, third);
-            }
-            */   
-            if (strcmp(first->symbol, SYM_LAMBDA) == 0) {
-       
-                Env *env = push_env(local);
-                Object* params = cadr(expr);
-                if(params->type!=TYPE_PAIR && params->type!=TYPE_NIL){
-                    DEBUG_PRINT_ERROR("TYPE ERROR: expected parameters\n");
-                    exit(1);
-                }
-                Object *body = caddr(expr);
-                /*
-                if(body->type!=TYPE_PAIR && body->type!=TYPE_INT){
-                    DEBUG_PRINT_ERROR("TYPE ERROR: expected function body\n");
-                    exit(1);
-                }
-                */
-                Object* lambda = make_lambda(params, body, env);
-                return lambda;
-            }
-
-            /* expect function call*/
-            Object *lambda = env_lookup(local, first->symbol);
-            if(lambda->type!=TYPE_LAMBDA){
-                DEBUG_PRINT_ERROR("TYPE ERROR: expected lambda expression\n");
-                exit(1);
-            }
-            Object* arg_list = cdr(expr);
-            Object* value = eval_function(lambda, arg_list);
-            return value;
-        }
-        /* Should have a lambda expression*/
-        if (first->type == TYPE_PAIR) {
-            Object* lambda = eval(local, first);
-            if(lambda->type!=TYPE_LAMBDA) {
-                DEBUG_PRINT_ERROR("TYPE ERROR: expected lambda expresions, got %s\n", type_to_string(lambda->type));
-                exit(1);               
-            }
-            Object *args = cdr(expr);
-            Object* value = eval_function(lambda, args);
-            return value;
-        }
-
-
-        printf("Cannot evaluate expression yet: ");
-        print_object(expr);
-        printf("\n");
-        exit(1);
+        return eval_list(local, expr);
     } else {
         return expr;
     }
