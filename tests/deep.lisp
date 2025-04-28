d7853f7fa89052778d7535c4fd171ac12f430757
joosypigeon <joosypigeon@googlemail.com>
2025-04-28 12:33:07 +0100

    correct deep tests

--- tests/deep.lisp
+++ tests/deep.lisp
@@ -1,3 +1,11 @@
 ;; Edge case: deeply nested expressions
-(print (if (equal? (((lambda (x) (lambda (y) (lambda (z) (+ x (+ y z))))) 1) 2 3) 6)
-        "pass: deep nest" "fail: deep nest"))
+(print 
+  (if 
+    (equal? 
+      ((((lambda (x) 
+           (lambda (y) 
+             (lambda (z) 
+               (+ x (+ y z))))) 1) 2) 3)
+      6)
+    "pass: deep nest"
+    "fail: deep nest"))
